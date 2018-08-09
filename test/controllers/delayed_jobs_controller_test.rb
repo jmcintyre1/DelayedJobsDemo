a925dbad1254f78dd341c684a21ebd92f42708ce
jmcintyre1 <jackson.mcintyre@ankura.com>
2018-08-09 13:16:02 -0400

    Added tests for index, show, delete.

--- test/controllers/delayed_jobs_controller_test.rb
+++ test/controllers/delayed_jobs_controller_test.rb
@@ -2,13 +2,21 @@ require 'test_helper'
 
 class DelayedJobsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get :index
+    get delayed_jobs_path
     assert_response :success
   end
 
   test "should show job" do
-    get delayed_job_path
+    get delayed_job_path(1)
     assert_response :success
+  end
+
+  test "should destroy job" do
+    assert_difference('Delayed::Job.all.count', -1) do
+      delete delayed_job_path(1)
+    end
+
+    assert_redirected_to delayed_jobs_path
   end
 
 end
